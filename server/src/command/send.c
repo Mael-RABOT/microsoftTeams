e1b3a252236ccdc13566ebd67d9af6211262cc08
Mael-RABOT <mael.rabot@epitech.eu>
2024-04-11 15:42:10 +0200

    wip
    Signed-off-by: Mael-RABOT <mael.rabot@epitech.eu>

--- server/src/command/send.c
+++ server/src/command/send.c
@@ -7,6 +7,25 @@
 
 #include "prototype.h"
 
+void save_message(char *message, uuid_t sender_uuid, uuid_t receiver_uuid)
+{
+    FILE *file;
+    char sender_uuid_str[37];
+    char receiver_uuid_str[37];
+    char filename[74 + 2];
+    char line[74 + 4 + strlen(message)]
+
+    uuid_unparse(sender_uuid, sender_uuid_str);
+    uuid_unparse(receiver_uuid, receiver_uuid_str);
+    sprintf(filename, "%s_%s", sender_uuid_str, receiver_uuid_str);
+    file = fopen(filename, "a");
+    if (file == NULL)
+        return;
+    sprintf(line, "%s %s %s\n", sender_uuid_str, receiver_uuid_str, message);
+    fwrite(line, 1, strlen(line), file);
+    fclose(file);
+}
+
 void send_command(server_t *server, user_t *user, packet_t *packet)
 {
     uuid_t target_uuid;
@@ -23,5 +42,6 @@ void send_command(server_t *server, user_t *user, packet_t *packet)
     } else {
         dprintf(dest_user->nsock, "%d: %s\n", OK, packet->args[1]);
     }
+    save_message(packet->args[1], user->uuid, target_uuid);
     dprintf(user->nsock, "%d: %s\n", OK, "OK");
 }
