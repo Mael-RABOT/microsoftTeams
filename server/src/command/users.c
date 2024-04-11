ccde618110841893dbfe774223416c4d26db6df4
Yohann Mangenot <yohann.mangenot@epitech.eu>
2024-04-11 15:41:08 +0200

    update(commands):add users
    Signed-off-by: Yohann Mangenot <yohann.mangenot@epitech.eu>

--- server/src/command/users.c
+++ server/src/command/users.c
@@ -2,13 +2,19 @@
 ** EPITECH PROJECT, 2024
 ** B-NWP-400-LYN-4-1-myteams-mael.rabot
 ** File description:
-** nothing.c
+** users.c
 */
 
 #include "server.h"
 
 void users_command(server_t *server, user_t *user, packet_t *packet)
 {
-    dprintf(user->nsock, "%d: USERS: Not implemented\n", NOT_IMPLEMENTED);
+    char **users = load_file(USER_FILE);
+
+    if (!users)
+        return;
+    for (int i = 0; users[i] != NULL; i++)
+        user->send(user, users[i]);
+    free(users);
     return;
 }
