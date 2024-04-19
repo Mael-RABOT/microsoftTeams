8b8531fdae5df0b9c72ab5639b6081ee7d155378
Mathieu <mathieu.borel@epitech.eu>
2024-04-19 10:04:54 +0200

    fix(server logger): fix sended messages
    Signed-off-by: Mathieu <mathieu.borel@epitech.eu>

--- server/src/command/create/channel.c
+++ server/src/command/create/channel.c
@@ -11,22 +11,18 @@ static void display_all_channel(user_t *user, va_list list)
 {
     channel_t *channel = va_arg(list, channel_t *);
 
-    user->send(user, "%d: Channel:%s:%s:%s\n", CREATED, channel->uuid_str,
+    user->send(user, "%d: %s:%s:%s\n", NOTIF_CHANNEL, channel->uuid_str,
         channel->name, channel->desc);
 }
 
 void create_server_channel(server_t *server, team_t *team, user_t *user,
     packet_t *packet)
 {
-    channel_t *channel = NULL;
+    channel_t *channel = create_channel();
 
-    if (len_array((void **)packet->args) < 2) {
+    if (len_array((void **)packet->args) < 2 || channel == NULL) {
+        delete_channel(channel);
         user->send(user, "%d: bad arguments\n", BAD_REQUEST);
-        return;
-    }
-    channel = create_channel();
-    if (channel == NULL) {
-        user->send(user, "%d: failed to create channel\n", BAD_REQUEST);
         return;
     }
     channel->team = team;
@@ -35,6 +31,8 @@ void create_server_channel(server_t *server, team_t *team, user_t *user,
     team->channels->push_back(team->channels, channel);
     server_event_channel_created(team->uuid_str, channel->uuid_str,
         channel->name);
+    user->send(user, "%d: Channel:%s:%s:%s\n", CREATED, channel->uuid_str,
+        channel->name, channel->desc);
     team->subscribed->foreach_arg(team->subscribed,
         (void (*)(void *, va_list))display_all_channel, channel);
 }
